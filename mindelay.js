48ba118ba6ae10f2232ea97f4811ab44e4a4a0fd
Clive Chan <cc@clive.io>
2016-08-02 18:20:25 -0400

    satisfy ESLint

--- mindelay.js
+++ mindelay.js
@@ -1,33 +1,35 @@
-"use strict";
 /*
-mindelay.js
+ * mindelay.js
+ *
+ * Defines the `mindelay` function. See `README.md` for details.
+ */
 
-Defines the `mindelay` function. See `README.md` for details.
-*/
+/* eslint no-invalid-this: "off" */
+/* eslint consistent-this: "off" */
+"use strict";
 
-/*eslint no-invalid-this: "off"*/
-/*eslint consistent-this: "off"*/
-
-function mindelay(callback, delayMS){
+function mindelay(callback, delayMS) {
   let self = this || null; //May be undefined. That's fine, we'll just go with the default apply() this-value of null.
 
-  if(typeof callback === "number" && typeof delayMS === "function")
+  if (typeof callback === "number" && typeof delayMS === "function")
     callback = [delayMS, delayMS = callback][0]; //wanted to write [callback, delayMS] = [delayMS, callback], but that's ES6
-  else if(typeof callback !== "function" || typeof delayMS !== "number")
+  else if (typeof callback !== "function" || typeof delayMS !== "number")
     throw new TypeError("mindelay expects a callback function and a delay");
 
   let endMS = (new Date().getTime()) + delayMS;
-  return function(){
+  return function() {
     let args = arguments;
-    if(typeof this !== "undefined") //the caller can bind a `this`, or otherwise it'll just default to the this from above.
+    if (typeof this !== "undefined") //the caller can bind a `this`, or otherwise it'll just default to the this from above.
       self = this;
     let now = new Date().getTime();
-    if(now < endMS)
-      setTimeout(function(){ callback.apply(self, args); }, endMS - now);
+    if (now < endMS)
+      setTimeout(function() {
+        callback.apply(self, args);
+      }, endMS - now);
     else
       callback.apply(self, args);
-  }
+  };
 }
 
-if(typeof module !== "undefined" && module.exports)
+if (typeof module !== "undefined" && module.exports)
   module.exports = mindelay;
--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
+/* eslint no-unused-expressions: "off" */
 "use strict";
 
 let expect = require("chai").expect;
 let mindelay = require("../mindelay.js");
 
-function caller(callback, delay){
-  setTimeout(function(){
+function caller(callback, delay) {
+  setTimeout(function() {
     callback("Some random arguments", undefined, 42);
   }, delay);
 }
@@ -12,40 +13,43 @@ function caller(callback, delay){
 describe("Callback timing with minimum delay", function() {
   const times = 5;
 
-  for(let i = 0; i <= times; i++) {
-    it("waits until one second is up when called in " + (i / times) +" seconds", function(done) {
-      this.timeout(1000 + 1000 / times);
+  for (let i = 0; i <= times; i++) {
+    it("delays until one second when called in " + (i / times) + " seconds",
+      function(done) {
+        this.timeout(1000 + 1000 / times);
 
-      let startTime = new Date().getTime();
-      let wrappedCallback = mindelay(function(){
-        let delta = new Date().getTime() - startTime;
-        expect(delta).to.be.within(995, 1020);
-        done();
-      }, 1000);
-      caller(wrappedCallback, 1000 / times *  i);
-    });
+        let startTime = new Date().getTime();
+        let wrappedCallback = mindelay(function() {
+          let delta = new Date().getTime() - startTime;
+          expect(delta).to.be.within(995, 1020);
+          done();
+        }, 1000);
+        caller(wrappedCallback, 1000 / times * i);
+      }
+    );
   }
 
-  for(let i = times + 1; i <= 2 * times; i++) {
-    it("does not wait when called in " + (i / times) +" seconds", function(done) {
-      this.timeout(1000 / times * (i+1));
+  for (let i = times + 1; i <= 2 * times; i++) {
+    it("does not delay when called in " + (i / times) + " seconds",
+      function(done) {
+        this.timeout(1000 / times * (i + 1));
 
-      let startTime = new Date().getTime();
-      let wrappedCallback = mindelay(function(){
-        let delta = new Date().getTime() - startTime;
-        let expected = 1000 / times * i;
-        expect(delta).to.be.within(expected - 5, expected + 20);
-        done();
-      }, 1000);
-      caller(wrappedCallback, 1000 / times *  i);
-    });
+        let startTime = new Date().getTime();
+        let wrappedCallback = mindelay(function() {
+          let delta = new Date().getTime() - startTime;
+          let expected = 1000 / times * i;
+          expect(delta).to.be.within(expected - 5, expected + 20);
+          done();
+        }, 1000);
+        caller(wrappedCallback, 1000 / times * i);
+      }
+    );
   }
 });
 
-
-describe("argument handling", function(){
-  it("allows the callback to receive arguments correctly", function(){
-    caller(function(a, b, c){
+describe("argument handling", function() {
+  it("allows the callback to receive arguments correctly", function() {
+    caller(function(a, b, c) {
       expect(a).to.equal("Some random arguments");
       expect(b).to.be.undefined;
       expect(c).to.equal(42);
@@ -53,6 +57,6 @@ describe("argument handling", function(){
   });
 });
 
-describe("`this` variable scoping", function(){
+describe("`this` variable scoping", function() {
 
 });
